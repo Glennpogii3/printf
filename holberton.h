9c88322e7949440c92c90ced0e6e377e2b38e8b7
Glennpogii3 <okafor21boo@gmail.com>
2022-05-18 09:59:04 +0800

    rot 13

--- holberton.h
+++ holberton.h
@@ -2,6 +2,26 @@
 #define HOLBERTON_H
 #define NULL ((void *)0)
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+
+/* utils.c */
+int _strlen(const char *);
+int print(char *);
+char *itoa(long int, int);
+
+/* printf.c */
+int _printf(const char *, ...);
+
+/* handler.c */
+int handler(const char *, va_list);
+int percent_handler(const char *, va_list, int *);
+
+
+int print_rot(va_list);
+
 int _putchar(char c);
 int _print_str(char *string);
 int _print_int(long int var);
@@ -10,5 +30,13 @@ int to_Binary(unsigned int n);
 int to_Octal(unsigned int n);
 int _power_recursion(int x, int y);
 int to_Hexa(unsigned int num);
+
+typedef struct _format
+{
+	char type;
+	int (*f)(va_list);
+} format;
+
+
 
 #endif
